4b3ed518d7cd3d9fadaba53db8d603db0dea9fc3
juespinosao <juespinosao@unal.edu.co>
2023-07-05 13:52:24 -0500

    Modificación para que el Código funcione con el mes siendo numero

--- R/imputacion.R
+++ R/imputacion.R
@@ -1,46 +1,46 @@
-#' Title
+#' Imputación outliers
 #'
-#'  @param mes Definir las tres primeras letras del mes a ejecutar, ej: Nov
-#'  @param anio Definir el año a ejecutar, ej: 2022
-#'  @param directorio definir el directorio donde se encuentran ubicado los datos de entrada
+#' Función para realizar la imputación por deuda y por casos especiales
+#' @param mes Definir las tres primeras letras del mes a ejecutar, ej: 11
+#' @param anio Definir el año a ejecutar, ej: 2022
+#' @param directorio definir el directorio donde se encuentran ubicado los datos de entrada
 #'
-#'  @return CSV file
-#'  @export
+#' @return CSV file
+#' @export
 #'
-#'  @examples imputacion_outliers(directorio="/Users/nataliaarteaga/Documents/DANE/Procesos DIMPE /PilotoEMMET",
+#' @examples imputacion_outliers(directorio="/Users/nataliaarteaga/Documents/DANE/Procesos DIMPE /PilotoEMMET",
 #'                        mes="nov",anio=2022)
 # Función identificación e imputación de outliers ------------------------------------------
 
 
 
 imputacion_outliers <- function(directorio,year,mes) {
+
+
+  # librerias ---------------------------------------------------------------
+
+
   library(dplyr)
-  #library(tidyr)
-  #library(plotly)
-  #library(plyr)
   library(readxl)
   library(readr)
   library(data.table)
   library(VIM)
-  meses <- c("ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic")
+  source("R/utils.R")
   month <- mes
   year  <- year
 
+  #cargar base estandarizada
   datos <- fread(paste0(directorio,"/results/S2_estandarizacion/EMMET_PANEL_estandarizado",meses[month],year,".csv"))
 
-  #necesita función identifiacion outliers
+  #crear una copia de la base de datos
   datoscom=datos
-  variablesinte=c("II_PA_PP_NPERS_EP","AJU_II_PA_PP_SUELD_EP","II_PA_TD_NPERS_ET",
-                  "AJU_II_PA_TD_SUELD_ET","II_PA_TI_NPERS_ETA","AJU_II_PA_TI_SUELD_ETA","II_PA_AP_AAEP","AJU_II_PA_AP_AAS_AP",
-                  "II_PP_PP_NPERS_OP","AJU_II_PP_PP_SUELD_OP","II_PP_TD_NPERS_OT","AJU_II_PP_TD_SUELD_OT","II_PP_TI_NPERS_OTA",
-                  "AJU_II_PP_TI_SUELD_OTA","II_PP_AP_APEP","AJU_II_PP_AP_AAS_PP","AJU_II_HORAS_HORDI_T","AJU_II_HORAS_HEXTR_T",
-                  "AJU_III_PE_PRODUCCION","AJU_III_PE_VENTASIN","AJU_III_PE_VENTASEX","III_EX_VEXIS")
-  datos=datos %>% select(ANIO,MES,ID_NUMORD,II_PA_PP_NPERS_EP,AJU_II_PA_PP_SUELD_EP,II_PA_TD_NPERS_ET,
+  #crear un data frame con las variables de interes
+  datos=datos %>% select(ANIO,MES,NOVEDAD,NOMBREDEPARTAMENTO,NOMBREMUNICIPIO,ID_NUMORD,NOMBRE_ESTAB,DOMINIOEMMET39,II_PA_PP_NPERS_EP,AJU_II_PA_PP_SUELD_EP,II_PA_TD_NPERS_ET,
                          AJU_II_PA_TD_SUELD_ET,II_PA_TI_NPERS_ETA,AJU_II_PA_TI_SUELD_ETA,II_PA_AP_AAEP,AJU_II_PA_AP_AAS_AP,
                          II_PP_PP_NPERS_OP,AJU_II_PP_PP_SUELD_OP,II_PP_TD_NPERS_OT,AJU_II_PP_TD_SUELD_OT,II_PP_TI_NPERS_OTA,
                          AJU_II_PP_TI_SUELD_OTA,II_PP_AP_APEP,AJU_II_PP_AP_AAS_PP,AJU_II_HORAS_HORDI_T,AJU_II_HORAS_HEXTR_T,
                          AJU_III_PE_PRODUCCION,AJU_III_PE_VENTASIN,AJU_III_PE_VENTASEX,III_EX_VEXIS)
-
+  #convertir la base en data frame y convertir variables de año y mes en numéricas
   datos<- as.data.frame(datos)
   datos$MES=as.numeric(datos$MES)
   datos$ANIO=as.numeric(datos$ANIO)
@@ -48,82 +48,147 @@ imputacion_outliers <- function(directorio,year,mes) {
     datos[,i] <- as.numeric(datos[,i])
     datos[,i] <- ifelse(is.na(datos[,i]),0,datos[,i])
   }
-
-  datos <- subset(datos, !(ANIO == year & MES == mes))
-
+  #Dejar la base sin los datos del mes actual
+  datos <- filter(datos, !(ANIO == year & MES == mes))
+  #cargar la base de alertas
   wowimp=fread(paste0(directorio,"/results/S3_identificacion_alertas/EMMET_PANEL_alertas_",meses[month],year,".csv"))
+  # Convertir los datos que son casos de imputación en NA
   for (i in variablesinte) {
     wowimp[wowimp[,paste0(i,"_caso de imputacion")]!="continua",i] <- NA
   }
-  wowimp=wowimp %>% select(ANIO,MES,ID_NUMORD,II_PA_PP_NPERS_EP,AJU_II_PA_PP_SUELD_EP,II_PA_TD_NPERS_ET,
+  wowimp=wowimp %>% select(ANIO,MES,NOVEDAD,NOMBREDEPARTAMENTO,NOMBREMUNICIPIO,ID_NUMORD,NOMBRE_ESTAB,DOMINIOEMMET39,II_PA_PP_NPERS_EP,AJU_II_PA_PP_SUELD_EP,II_PA_TD_NPERS_ET,
                            AJU_II_PA_TD_SUELD_ET,II_PA_TI_NPERS_ETA,AJU_II_PA_TI_SUELD_ETA,II_PA_AP_AAEP,AJU_II_PA_AP_AAS_AP,
                            II_PP_PP_NPERS_OP,AJU_II_PP_PP_SUELD_OP,II_PP_TD_NPERS_OT,AJU_II_PP_TD_SUELD_OT,II_PP_TI_NPERS_OTA,
                            AJU_II_PP_TI_SUELD_OTA,II_PP_AP_APEP,AJU_II_PP_AP_AAS_PP,AJU_II_HORAS_HORDI_T,AJU_II_HORAS_HEXTR_T,
                            AJU_III_PE_PRODUCCION,AJU_III_PE_VENTASIN,AJU_III_PE_VENTASEX,III_EX_VEXIS)
-
+  #Combinar la base sin el mes actual con la base de alertas
   base_imputar=rbind(datos,wowimp)
 
 
   # imputacion ---------------------------------------------------------------------
 
+  #crear un data frame con el valor del mes anterior y del año anterior
+  tra <- base_imputar  %>% group_by(ID_NUMORD) %>%
+    mutate_at(c(variablesinte),.funs=list(rezago1=~lag(.),mes_anio_ant=~ifelse(sum(MES==mes & ANIO==(year-1))>=1,.[MES==mes & ANIO==year-1],NA)))#,
+  tra <- tra  %>% filter(ANIO==year & MES==mes)%>%
+    as.data.frame()
+
+  #calcular la variación que hubo con respecto al mes anterior
+  for (i in variablesinte) {
+    tra[,paste0(i,"_var_mes_ant")]=(tra[,i]-tra[,paste0(i,"_rezago1")])/tra[,paste0(i,"_rezago1")]
+    tra[tra[,i]==0 & !is.na(tra[,i]) & tra[,paste0(i,"_rezago1")]==0& !is.na(tra[,paste0(i,"_rezago1")]),paste0(i,"_var_mes_ant")] <- 0
+    tra[tra[,i]!=0 & !is.na(tra[,i]) & tra[,paste0(i,"_rezago1")]==0 & !is.na(tra[,paste0(i,"_rezago1")]),paste0(i,"_var_mes_ant")] <- 1
+  }
+
+  #Convertir algunas variables como factor para la función KNN
+  base_imputar$ANIO=as.factor(base_imputar$ANIO)
+  base_imputar$MES=as.factor(base_imputar$MES)
+  base_imputar$ID_NUMORD=as.factor(base_imputar$ID_NUMORD)
+  base_imputar$DOMINIOEMMET39=as.factor(base_imputar$DOMINIOEMMET39)
+
+  # realizar la imputación de las variables de interes con el comando kNN
   set.seed(11)
-  wowKNN <- base_imputar %>%
+  wowKNN4md <- base_imputar %>%
     group_by(ID_NUMORD) %>%
     group_modify(~ kNN(.x,numFun = mean,k=4))
 
-  wowKNN=wowKNN%>% filter(MES==mes & ANIO==year) %>%
+  #filtrar por los valores del mes actual y ordenar por "ID_NUMORD"
+  wowKNN4md=wowKNN4md%>% filter(MES==mes & ANIO==year) %>%
     arrange(ID_NUMORD) %>%
     as.data.frame()
 
+  #crea un data frame con las variaciones y convierte algunas variables como anio,mes, id_numord en factor
+  traf1=tra %>% select(ANIO,MES,ID_NUMORD,DOMINIOEMMET39,ends_with("mes_ant"))
+  traf1$ANIO=as.factor(traf1$ANIO)
+  traf1$MES=as.factor(traf1$MES)
+  traf1$ID_NUMORD=as.factor(traf1$ID_NUMORD)
+  traf1$DOMINIOEMMET39=as.factor(traf1$DOMINIOEMMET39)
+  #Vector con los nombres de las variables que contienen los valores de la variación de las variables del capitulo 3
+  capf=c("AJU_III_PE_PRODUCCION_var_mes_ant","AJU_III_PE_VENTASIN_var_mes_ant","AJU_III_PE_VENTASEX_var_mes_ant","III_EX_VEXIS_var_mes_ant" )
+
+  #realiza un segundo KNN para imputar la variación del mes anterior
+  set.seed(11)
+  wowKNNci3 <- traf1 %>%
+    group_by(DOMINIOEMMET39) %>%
+    group_modify(~ kNN(.x,numFun = median,k=6))
+
+  wowKNNci3=wowKNNci3%>% filter(MES==mes & ANIO==year) %>%
+    arrange(ID_NUMORD) %>%
+    as.data.frame()
+
+  #crea un data frame combinando el KNN de la imputación de las variables y de la variación
+  wowKNNciu3=cbind(wowKNN4md,wowKNNci3[,capf])
 
 
+  #crea el valor final multiplicando el valor de la imputación de las variables por 1 más el valor de la
+  #variación imputada
+  for (i in cap3) {
+    filas_completas <- !(complete.cases(wowimp[,paste0(i)]))
+    wowKNNciu3[filas_completas,i]<- wowKNNciu3[filas_completas,i]*(1+ wowKNNciu3[filas_completas,paste0(i,"_var_mes_ant")])
+
+  }
+  #convierte el id_numord en factor
+  tra$ID_NUMORD <- as.factor(tra$ID_NUMORD)
+
+
+  # imputación por deuda ----------------------------------------------------
+  wowKNNciu3= wowKNNciu3 %>%
+    left_join(select(tra,c("ID_NUMORD",ends_with("rezago1"))), by = "ID_NUMORD")
+  #valor para los establecimientos imputados por deuda multiplicando el valor del mes anterior por
+  # 1 más el valor de la variación imputada
+  for (i in cap3) {
+    filas_completas <- which(wowimp$NOVEDAD==5)
+    wowKNNciu3[filas_completas,i]<- wowKNNciu3[filas_completas,paste0(i,"_rezago1")]*(1+ wowKNNciu3[filas_completas,paste0(i,"_var_mes_ant")])
+
+  }
+
+
+  #crear una base con los valores del mes anterior
   tra <- base_imputar  %>% group_by(ID_NUMORD) %>%
     mutate_at(c(variablesinte),.funs=list(rezago1=~lag(.)))#,
 
-  tra <- tra  %>% filter(MES==mes & ANIO==year)
-
-
-
-  mes_ant=tra
+  mes_ant <- tra  %>% filter(MES==mes & ANIO==year)
 
   mes_ant=as.data.frame(mes_ant)
 
-  cap2=c("II_PA_PP_NPERS_EP","AJU_II_PA_PP_SUELD_EP","II_PA_TD_NPERS_ET",
-         "AJU_II_PA_TD_SUELD_ET","II_PA_TI_NPERS_ETA","AJU_II_PA_TI_SUELD_ETA","II_PA_AP_AAEP","AJU_II_PA_AP_AAS_AP",
-         "II_PP_PP_NPERS_OP","AJU_II_PP_PP_SUELD_OP","II_PP_TD_NPERS_OT","AJU_II_PP_TD_SUELD_OT","II_PP_TI_NPERS_OTA",
-         "AJU_II_PP_TI_SUELD_OTA","II_PP_AP_APEP","AJU_II_PP_AP_AAS_PP","AJU_II_HORAS_HORDI_T","AJU_II_HORAS_HEXTR_T")
 
   for (j in cap2) {
-
+    #identificar las filas que tienen NA
     missing_values <- is.na(mes_ant[, j])
 
-    # Assign values to the selected rows in column j
+    #imputar las variables del capítulo 2 por el valor del mes anterior
     mes_ant[missing_values, j] <- round(mes_ant[missing_values, paste0(j,"_rezago1")])
 
   }
 
+  #filtrar solo por el mes de interés y dejar las variables importantes
   mes_ant=mes_ant%>% filter(MES==mes & ANIO==year) %>%
     arrange(ID_NUMORD)
 
-  mes_ant=mes_ant %>% select(ANIO,MES,ID_NUMORD,II_PA_PP_NPERS_EP,AJU_II_PA_PP_SUELD_EP,II_PA_TD_NPERS_ET,
+  mes_ant=mes_ant %>% select(ANIO,MES,NOVEDAD,NOMBREDEPARTAMENTO,NOMBREMUNICIPIO,ID_NUMORD,NOMBRE_ESTAB,DOMINIOEMMET39,II_PA_PP_NPERS_EP,AJU_II_PA_PP_SUELD_EP,II_PA_TD_NPERS_ET,
                              AJU_II_PA_TD_SUELD_ET,II_PA_TI_NPERS_ETA,AJU_II_PA_TI_SUELD_ETA,II_PA_AP_AAEP,AJU_II_PA_AP_AAS_AP,
                              II_PP_PP_NPERS_OP,AJU_II_PP_PP_SUELD_OP,II_PP_TD_NPERS_OT,AJU_II_PP_TD_SUELD_OT,II_PP_TI_NPERS_OTA,
                              AJU_II_PP_TI_SUELD_OTA,II_PP_AP_APEP,AJU_II_PP_AP_AAS_PP,AJU_II_HORAS_HORDI_T,AJU_II_HORAS_HEXTR_T,
                              AJU_III_PE_PRODUCCION,AJU_III_PE_VENTASIN,AJU_III_PE_VENTASEX,III_EX_VEXIS)
 
-  cap3=c("AJU_III_PE_PRODUCCION","AJU_III_PE_VENTASIN","AJU_III_PE_VENTASEX","III_EX_VEXIS" )
 
-  mes_ant <- mes_ant[, !(names(mes_ant) %in% cap3)]
-  mes_ant <- cbind(mes_ant, wowKNN[, cap3])
+  #quitar las columnas del capítulo 3 para imputarlos por el valor optenido con los KNN
   mes_ant=arrange(mes_ant,mes_ant$ID_NUMORD)
-  novtem=subset(datoscom, (ANIO == year & MES == mes))
+  mes_ant <- mes_ant[, !(names(mes_ant) %in% cap3)]
+  mes_ant <- cbind(mes_ant, wowKNNciu3[, cap3])
+
+  #crear una base con el mes de interes y ordenar por el id_numord
+  novtem=filter(datoscom, (ANIO == year & MES == mes))
   novtem=arrange(novtem,novtem$ID_NUMORD)
 
+  #convertir algunas variables en numericas
   novtem$II_PA_PP_NPERS_EP<-as.numeric(novtem$II_PA_PP_NPERS_EP)
   novtem$AJU_III_PE_VENTASEX<-as.numeric(novtem$AJU_III_PE_VENTASEX)
   novtem$AJU_II_HORAS_HEXTR_T<-as.numeric(novtem$AJU_II_HORAS_HEXTR_T)
   novtem=as.data.frame(novtem)
-  datoscom <- subset(datoscom, !(ANIO == year & MES == mes))
+
+  #Modificar la base copia para que no tenga el mes de interes
+  datoscom <- filter(datoscom, !(ANIO == year & MES == mes))
   datoscom<- as.data.frame(datoscom)
   datoscom$MES=as.numeric(datoscom$MES)
   datoscom$ANIO=as.numeric(datoscom$ANIO)
@@ -132,19 +197,26 @@ imputacion_outliers <- function(directorio,year,mes) {
     datoscom[,i] <- ifelse(is.na(datoscom[,i]),0,datoscom[,i])
   }
 
+  #pegar las columnas imputadas en el data frame del mes actual
   for (i in variablesinte){
     novtem[,i]<- mes_ant[,i]
 
   }
+
+  #combinar las base original estandarizada con el mes imputado
+
   imputa=rbind(datoscom,novtem)
   imputa=as.data.frame(imputa)
   for (i in variablesinte) {
     imputa[,i] <- as.numeric(imputa[,i])
   }
   imputa <- imputa %>% mutate_if(is.integer, as.numeric)
+
+
+
+  # Exportar la base imputada -------------------------------
+
   write.csv(imputa,paste0(directorio,"/results/S4_imputacion/EMMET_PANEL_imputada_",meses[month],year,".csv"),row.names=F)
 }
-
-
 
 
