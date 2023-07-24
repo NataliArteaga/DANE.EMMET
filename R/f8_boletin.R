22f484581d96a2487e3f78ea7e27575e373efef9
juespinosao <juespinosao@unal.edu.co>
2023-07-24 17:12:44 -0500

    modificar parametros

--- R/f8_boletin.R
+++ R/f8_boletin.R
@@ -1,15 +1,13 @@
-boletin <- function(directorio,
+f8_boletin <- function(directorio,
                     mes,
-                    anio,
-                    fecha,
-                    guardar){
+                    anio){
   library(rmarkdown)
   rmarkdown::render("/Users/nataliaarteaga/Documents/DANE/Procesos DIMPE /Functions/S4_boletin/boletin.Rmd", "pdf_document",
                     params = list(month= mes,
                                   year= anio,
-                                  fecha_publicacion= fecha,
+                                  fecha_publicacion= Sys.Date(),
                                   directorio= directorio),
                     output_file = paste0("boletin_",Sys.Date()),
-                    output_dir = guardar)
+                    output_dir = paste0(directorio,"/results/S7_boletin"))
 }
 
