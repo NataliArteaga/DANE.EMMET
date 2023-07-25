d98c72738e0579737a142db7d1e7342378efc187
juespinosao <juespinosao@unal.edu.co>
2023-07-24 20:36:50 -0500

    arreglos boletin

--- R/f8_boletin.R
+++ R/f8_boletin.R
@@ -1,15 +1,24 @@
-f8_boletin <- function(directorio,
-                    mes,
-                    anio,
-                    tipo="pdf"){
+f8_boletin <- function(directorio, mes, anio, tipo = "pdf") {
   library(rmarkdown)
-  link="https://github.com/NataliArteaga/DANE.EMMET/raw/main/boletin/boletin.Rmd"
-  rmarkdown::render(link,paste0(tipo,"_document"),
-                    params = list(month= mes,
-                                  year= anio,
-                                  fecha_publicacion= Sys.Date(),
-                                  directorio= directorio),
-                    output_file = paste0("boletin_",Sys.Date()),
-                    output_dir = paste0(directorio,"/results/S7_boletin"))
+
+  # Definir la URL de la carpeta boletin en GitHub
+  url_boletin <- "https://github.com/NataliArteaga/DANE.EMMET/raw/main/boletin.zip"
+
+  # Descargar y descomprimir la carpeta boletin
+  archivo_zip <- file.path(directorio, "boletin.zip")
+  download.file(url_boletin, destfile = archivo_zip)
+  unzip(archivo_zip, exdir = file.path(directorio, "boletin"))
+
+  # Definir la ruta del archivo boletin.Rmd en la carpeta descargada
+  ruta_boletin_rmd <- file.path(directorio, "boletin","boletin", "boletin.Rmd")
+
+  # Renderizar el archivo Rmd localmente
+  rmarkdown::render(ruta_boletin_rmd, paste0(tipo, "_document"),
+                    params = list(month = mes,
+                                  year = anio,
+                                  fecha_publicacion = Sys.Date(),
+                                  directorio = directorio),
+                    output_file = paste0("boletin_", Sys.Date()),
+                    output_dir = file.path(directorio, "results", "S7_boletin"))
 }
 
