961dda0ae69e80538606161b6793bbc4eb03dd28
juespinosao <juespinosao@unal.edu.co>
2023-07-26 09:05:07 -0500

    corrección documentación boletin y arreglo codigo

--- R/f8_boletin.R
+++ R/f8_boletin.R
@@ -1,25 +1,26 @@
-#' Macro 1
+#' Boletin
 #'
-#' Con esta función realizaremos la primera parte del proceso, en donde se correran las 4
-#' primeras funciones.
+#' @description Está función crea el boletín, un documento exportado en pdf o word, según se desea.
+#'
+#'  Este es un resumen ejecutivo en el que se presentan los
+#'  principales resultados con el uso de cuadros de líneas para presentar
+#'  información histórica y graficas de barras para hacer comparaciones de
+#'  variaciones entre las distintas actividades o desagregación regional.
+#'  Además, se presentan tablas con el resumen por actividad para el total
+#'  nacional, departamentos, áreas metropolitanas y ciudades incluyendo
+#'  las tres variables principales que permiten hacer un análisis cruzado
+#'  entre estas.
 #'
 #'
 #' @param mes Definir el mes a ejecutar, ej: 11
 #' @param anio Definir el año a ejecutar, ej: 2022
 #' @param directorio definir el directorio donde se encuentran ubicado los datos de entrada
+#' @param tipo Definir si se quiere un archivo pdf, word, o html, por defecto esta la opción "pdf", ej: "html
 #'
-#' @details  Esta funcion tiene como objetivo correr desde la función inicial hasta la función
-#' de identificacion de alertas, con el fin de que el usuario pueda obtener rapidamente el
-#' archivo de salida de la función de alertas y poder realizar el proceso de critica.
-#' Recuerde que si el archivo de alertas ya está creado, al correr esta función le pedira que
-#' responda S si quiere sobreescribirlo, de lo contrario otro valor para cancelar la función
-#'
-#' Ver:\code{\link{f0_inicial}},\code{\link{f1_integracion}}, \code{\link{f2_estandarizacion}},
-#' \code{\link{f3_identificacion_outliers}}
+
 #'
 #'
-#' @examples macro1(directorio="Documents/DANE/Procesos DIMPE /PilotoEMMET",
-#'                        mes=11,anio=2022)
+#' @examples f8_boletin(directorio"Documents/DANE/Procesos DIMPE /PilotoEMMET",mes = 11  ,anio =2022,tipo="word")
 
 
 
@@ -35,6 +36,7 @@
 f8_boletin <- function(directorio, mes, anio, tipo = "pdf") {
   library(rmarkdown)
   library(readxl)
+  library(lubridate)
   # Definir la URL de la carpeta boletin en GitHub
   url_boletin <- "https://github.com/NataliArteaga/DANE.EMMET/raw/main/boletin.zip"
 
@@ -46,11 +48,26 @@ f8_boletin <- function(directorio, mes, anio, tipo = "pdf") {
   # Definir la ruta del archivo boletin.Rmd en la carpeta descargada
   ruta_boletin_rmd <- file.path(directorio, "boletin","boletin", "boletin.Rmd")
   parametros <- as.data.frame(read_excel(paste0(directorio,"/results/S7_boletin/parametros_boletin.xlsx")))
+  meses_b <- c("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre")
+
+
+  # Creamos una función para obtener el nombre del mes en español
+  nombre_mes_espanol <- function(fecha) {
+    return(meses_b[month(fecha)])
+  }
+
+  # Formateamos la fecha en el formato deseado
+  fecha_formateada <- paste(day(Sys.Date()), "de", nombre_mes_espanol(Sys.Date()), "de", year(Sys.Date()))
+
+
+
+
   # Renderizar el archivo Rmd localmente
   rmarkdown::render(ruta_boletin_rmd, paste0(tipo, "_document"),
                     params = list(month = mes,
                                   year = anio,
-                                  fecha_publicacion = Sys.Date(),
+                                  month_b= meses_b[mes],
+                                  fecha_publicacion = fecha_formateada,
                                   directorio = directorio,
                                   IC_prod= parametros[1,2],
                                   IC_ven= parametros[2,2],
@@ -60,7 +77,7 @@ f8_boletin <- function(directorio, mes, anio, tipo = "pdf") {
                                   TI_ven=parametros[6,2],
                                   TI_empl= parametros[7,2],
                                   ),
-                    output_file = paste0("boletin_", Sys.Date()),
+                    output_file = paste0("boletin_", fecha_formateada),
                     output_dir = file.path(directorio, "results", "S7_boletin"))
 }
 
